e67d724c57c6f11a77d68c2627e8928eadfc16d1
RajrupDasid <rajrupofficialid@protonmail.com>
2023-09-05 11:37:56 +0530

    Initial Commit
    Review Phase 1
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+// toggle the menu and navigation on mobile
+const menu = document.querySelector('.menu');
+const navMobile = document.querySelector('.nav-mobile');
+
+// function toggleMenu(){
+//     menu.classList.toggle('active');
+//     navMobile.classList.toggle('active');
+// }
+const toggleMenu=()=>{
+    menu.classList.toggle('active');
+    navMobile.classList.toggle('active')
+}
